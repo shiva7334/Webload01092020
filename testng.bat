47711dc1b0a9ac5caae443f2aed30ed8f983d268
shiva7334 <59619449+shiva7334@users.noreply.github.com>
2020-01-09 20:55:18 -0600

    Add files via upload

--- testng.bat
+++ testng.bat
@@ -1,4 +1,4 @@
-set projectLocation=https://github.com/shiva7334/Webload01092020.git
+set projectLocation=C:\Users\Sruthi\git\Webload01092020\Webload
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
